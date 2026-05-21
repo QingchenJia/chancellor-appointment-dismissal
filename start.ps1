dbd2f532dfebe88d40a1d383dd8c447400f89f48
Qingchen_Jia <qingchenjia1227@qq.com>
2026-05-21 23:12:54 +0800

    chore: update local dev ignore and startup script

--- start.ps1
+++ start.ps1
@@ -1,8 +1,1 @@
-Write-Host ""
-Write-Host "Server will be available at:"
-Write-Host "  http://localhost:8000"
-Write-Host ""
-Write-Host "Press Ctrl+C to stop the server."
-Write-Host ""
-
 conda run -n document python -m uvicorn song_chancellors.api:create_app --factory --host 127.0.0.1 --port 8000
